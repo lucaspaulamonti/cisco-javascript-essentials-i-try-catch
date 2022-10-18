97417c7aaa2176561d29fea2451c498c45bc3c96
lucaspaulamonti <lucasdepaulamonti@gmail.com>
2022-10-18 00:17:18 -0300

    Create try-catch.js

---
+++ try-catch.js
@@ -0,0 +1,7 @@
+// Utilizando try e catch para não interromper a execução do programa.
+try {
+    console.log('abc');
+    conole.log('abc');
+} catch (error) {  
+    console.log(error.message);
+}
